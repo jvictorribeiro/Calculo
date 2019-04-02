dd77f6005224eaf65ffe016731138588b1109111
Victor Ribeiro <44476242+jvictorribeiro@users.noreply.github.com>
2019-04-01 21:27:33 -0300

    finalV
    conserto dos prints

--- bissec.py
+++ bissec.py
@@ -11,7 +11,7 @@ def bisection(a, b):
 
     c = a
     # enquanto for maior que o erro faz a iteraçao
-    while((b - a) >= 0.00001): 
+    while((b - a) >= 0.00001):
 
         #Encontra o valor mediano
         c = (a + b) / 2
@@ -20,14 +20,17 @@ def bisection(a, b):
         if(f(c) == 0.0):
             break
 
+        print("O valor de a eh : ", "%.6f" % a)
+        print("O valor de b eh : ", "%.6f" % b)
+
         # Decide o lado para repetir os passos anteriores
         if(f(c) * f(a) < 0):
             b = c
         else:
             a = c
-        print("O valor de a eh : ", "%.6f" % a)
-        print("O valor de b eh : ", "%.6f" % b)
+
         print("O valor da raiz eh : ", "%.6f" % c)
+        print("O valor de f(cn) : ", "%.6f" % f(c))
 
     print("O valor final da raiz eh : ", "%.6f" % c)
 
