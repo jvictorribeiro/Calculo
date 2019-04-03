0687ab0be4ed477b5d7235ded7a1442aabe7f8a2
Victor Ribeiro <44476242+jvictorribeiro@users.noreply.github.com>
2019-04-03 17:44:48 -0300

    finalV

--- agentR.py
+++ agentR.py
@@ -1,26 +1,22 @@
 #Agente reativo simples em python
-
 import time 
 from random import randint
 
 agente = "@"
-
 limpo = 0
 
 ambiente = [[1,4,0,0,2,0],
-			[0,0,3,1,0,4],		#definicao do ambiente de tarefa
+			[0,0,3,1,0,4],			#definicao do ambiente de tarefa
 			[0,1,0,0,2,0],
 			[0,0,2,1,0,1],
 			[2,0,1,0,0,4]]
-
 
 def printar(ambiente):		#printa o ambiente
 	for secao in ambiente:
 		print(secao)
 	print("\n")
 
-
-def sujaAmbiente(ambiente):	#gera um ambiente com novas sujeiras
+def sujaAmbiente(ambiente):		#gera um ambiente com novas sujeiras
 	for secao in ambiente:
 		atual = 0
 		for local in secao:
@@ -29,15 +25,14 @@ def sujaAmbiente(ambiente):	#gera um ambiente com novas sujeiras
 
 	return ambiente
 
-
 def agentAspirador(ambiente):
-
 	pontuacao = 0
 	for secao in ambiente:
 		atual = 0
 		for local in secao:
 
-			if local != limpo:	#se local for sujo, limpa
+			#se local for sujo, limpa
+			if local != limpo:		
 
 				secao[atual] = agente
 
@@ -50,7 +45,8 @@ def agentAspirador(ambiente):
 				print 'Pontuacao do agente:', pontuacao
 				atual += 1
 
-			else:	#se n for sujo salva o local atual, coloca o agente no local e depois restaura
+			#se n for sujo salva o local atual, coloca o agente no local e depois restaura
+			else:			
 				salva = secao[atual]
 				secao[atual] = agente
 				printar(ambiente)
@@ -58,17 +54,17 @@ def agentAspirador(ambiente):
 				time.sleep(0.5)
 				#se vai para lugar sem sujeira perde ponto
 				pontuacao += -1
-				print 'Pontuacao do agente:', pontuacao'
+				print 'Pontuacao do agente:', pontuacao
 				atual += 1
 				continue
 
-	return pontuacao
+	return ambiente
 
 #main
 ambiente = sujaAmbiente(ambiente)	#novo ambiente sujo
 printar(ambiente)
-print(agentAspirador(ambiente))		
-
+agentAspirador(ambiente)		
+printar(ambiente)
 
 
 
