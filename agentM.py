e01aa57d6e2e4cbf9a52fb6b61e2e8c0b6e7b983
Victor Ribeiro <44476242+jvictorribeiro@users.noreply.github.com>
2019-04-03 19:17:03 -0300

    FinalV
    pontuacao corrected

--- agentM.py
+++ agentM.py
@@ -7,8 +7,8 @@ areiaMove = 1	#agente tem que pular
 buraco = 2		#agente deve tomar distancia e pular
 ouro = 3
 
-ambiente = [[0,1,0,0,1,0],
-			[0,2,0,0,2,0],			#definicao do ambiente de tarefa
+ambiente = 		[[0,1,0,0,1,0],
+			[0,2,0,0,2,0],		#definicao do ambiente de tarefa
 			[0,1,0,0,1,0],
 			[0,2,0,0,2,0],
 			[0,1,0,0,1,3]]
@@ -24,7 +24,6 @@ def agent(ambiente):
 		atual = 0
 		for local in secao:
 
-
 			#se local for igual a caminho, ele caminha
 			if local == caminho:			
 				secao[atual] = agente
@@ -38,7 +37,7 @@ def agent(ambiente):
 
 			#se local para onde ele está tentando ir for areia, ele pula
 			elif local == areiaMove:	
-				#se pula, perde 1 pontos
+				#se pula, perde 1 ponto
 				pontuacao += -1
 				#print ('Pontuacao do agente:', pontuacao)
 				atual += 1
@@ -55,7 +54,7 @@ def agent(ambiente):
 				printar(ambiente)
 				secao[atual-1] = salva
 				time.sleep(1)
-				#se volta para pular, perde 3 pontos
+				#se volta para pular, perde 2 pontos
 				pontuacao += -2
 				print ('\nPontuacao do agente:', pontuacao)
 				atual += 1
@@ -64,7 +63,6 @@ def agent(ambiente):
 				print ('Agente conseguiu pegar o ouro')
 				pontuacao += 5
 				print ('Pontuacao final:', pontuacao)
-
 	return ambiente
 
 #main
