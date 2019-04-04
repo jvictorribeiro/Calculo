489ba8477613dab6f25a01b52f9e38bb2bf9f307
Victor Ribeiro <44476242+jvictorribeiro@users.noreply.github.com>
2019-04-03 22:54:24 -0300

    finalV

--- agentR.py
+++ agentR.py
@@ -4,6 +4,7 @@ from random import randint
 
 agente = "@"
 limpo = 0
+numSujeira = 0
 
 ambiente = [[1,4,0,0,2,0],
 			[0,0,3,1,0,4],			#definicao do ambiente de tarefa
@@ -20,20 +21,22 @@ def sujaAmbiente(ambiente):		#gera um ambiente com novas sujeiras
 	for secao in ambiente:
 		atual = 0
 		for local in secao:
-			secao[atual] = randint(0,4)
+			secao[atual] = randint(0,7)
 			atual += 1
 
 	return ambiente
 
 def agentAspirador(ambiente):
 	pontuacao = 0
+	global numSujeira
 	for secao in ambiente:
 		atual = 0
 		for local in secao:
 
-			#se local for sujo, limpa
-			if local != limpo:		
 
+			#se local for sujo, limpa
+			if (local == 1) or (local == 2) or (local == 3) or (local == 4):		
+				numSujeira += 1
 				secao[atual] = agente
 
 				printar(ambiente)
@@ -45,8 +48,8 @@ def agentAspirador(ambiente):
 				print 'Pontuacao do agente:', pontuacao
 				atual += 1
 
-			#se n for sujo salva o local atual, coloca o agente no local e depois restaura
-			else:			
+			elif (local == 5) or (local == 6) or (local == 7):
+				numSujeira += 1
 				salva = secao[atual]
 				secao[atual] = agente
 				printar(ambiente)
@@ -56,7 +59,16 @@ def agentAspirador(ambiente):
 				pontuacao += -1
 				print 'Pontuacao do agente:', pontuacao
 				atual += 1
-			
+
+			#se n for sujo salva o local atual, coloca o agente no local e depois restaura
+			else:			
+				salva = secao[atual]
+				secao[atual] = agente
+				printar(ambiente)
+				secao[atual] = salva
+				time.sleep(0.5)
+				atual += 1
+
 	return ambiente
 
 #main
@@ -64,6 +76,4 @@ ambiente = sujaAmbiente(ambiente)	#novo ambiente sujo
 printar(ambiente)
 agentAspirador(ambiente)		
 printar(ambiente)
-
-
-
+print 'Numero de sujeiras:', numSujeira
